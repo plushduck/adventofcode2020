47a0b0cea82ec983b759e708883835648600865b
phillipalexander <phillipalexander@granular.ag>
2020-12-02 09:32:29 -0600

    Day 2 part 2

--- 2/solution.py
+++ 2/solution.py
@@ -6,17 +6,26 @@ def read_test_file(filename):
     with open(filename) as f:
         return f.read().splitlines()
 
-test_parser = re.compile(r'(?P<min>.*)-(?P<max>.*) (?P<char>.+): (?P<pass>.*)')
-
-def password_valid(test_str):
-    m = test_parser.match(test_str)
-    c = Counter(m.group(4))
+test_parser_a = re.compile(r'(?P<min>.*)-(?P<max>.*) (?P<char>.+): (?P<pass>.*)')
+def password_valid_a(test_str):
+    m = test_parser_a.match(test_str)
+    c = Counter(m.group('pass'))
     valid = c[m.group('char')] >= int(m.group('min')) and c[m.group('char')] <= int(m.group('max'))
-    print(f"test = {test_str}, {valid}")
+    # print(f"test = {test_str}, {valid}")
+    return valid
+
+test_parser_b = re.compile(r'(?P<pos_1>.*)-(?P<pos_2>.*) (?P<char>.+): (?P<pass>.*)')
+def password_valid_b(test_str):
+    m = test_parser_b.match(test_str)
+    pw = m.group('pass')
+    char = m.group('char')[0]
+    pos_1 = int(m.group('pos_1'))-1
+    pos_2 = int(m.group('pos_2'))-1
+    valid = (pw[pos_1] == char) ^ (pw[pos_2] == char)
+    # print(f"test = {test_str}, {valid}")
     return valid
 
 if __name__ == '__main__':
     tests = read_test_file('2/input_1.txt')
-    valid_count = len(list(filter(password_valid, tests)))
-    print(valid_count)
-
+    print(len(list(filter(password_valid_a, tests))))
+    print(len(list(filter(password_valid_b, tests))))
