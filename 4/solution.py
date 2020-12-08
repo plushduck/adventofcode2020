aebabd3397cc7ed2dcc60f3ba13ff757fbf67bda
phillipalexander <phillipalexander@granular.ag>
2020-12-08 13:26:31 -0600

    Day 4, part 2

--- 4/solution.py
+++ 4/solution.py
@@ -18,6 +18,8 @@ hcl:#cfa07d eyr:2025 pid:166559648
 iyr:2011 ecl:brn hgt:59in
 '''
 
+
+
 @dataclass
 class Passport:
     byr: str # Birth Year
@@ -29,16 +31,57 @@ class Passport:
     pid: str # Passport ID
     cid: str = "" # Country ID (optional)
 
-    def is_valid():
-
 
 field_pair_parser = re.compile(r'(?P<name>.*):(?P<value>.*)')
 
+'''
+Field rules
+
+byr (Birth Year) - four digits; at least 1920 and at most 2002.
+iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+hgt (Height) - a number followed by either cm or in:
+If cm, the number must be at least 150 and at most 193.
+If in, the number must be at least 59 and at most 76.
+hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+pid (Passport ID) - a nine-digit number, including leading zeroes.
+cid (Country ID) - ignored, missing or not.
+'''
+FIELD_PARSERS = {
+    'byr': re.compile(r'\d{4}'),
+    'iyr': re.compile(r'\d{4}'),
+    'eyr': re.compile(r'\d{4}'),
+    'hgt': re.compile(r'(\d{3}cm|\d{2}in)'),
+    'hcl': re.compile(r'#[0-9a-f]{6}'),
+    'ecl': re.compile(r'(amb|blu|brn|gry|grn|hzl|oth)'),
+    'pid': re.compile(r'\d{9}')
+}
+
+def year_field_valid(kwargs, name, min_year, max_year):
+    year = int(kwargs[name])
+    return year >= min_year and year <= max_year
+
 def passport_args_valid(kwargs):
-    try:
-        Passport(kwargs)
-    except TypeError:
+    for k,v in kwargs.items():
+        if k in FIELD_PARSERS:
+            if not FIELD_PARSERS[k].fullmatch(v):
+                return False
+
+    if not year_field_valid(kwargs, 'byr', 1920, 2002):
         return False
+    if not year_field_valid(kwargs, 'iyr', 2010, 2020):
+        return False
+    if not year_field_valid(kwargs, 'eyr', 2020, 2030):
+        return False
+    if kwargs['hgt'][-2:] == 'cm':
+        hgt = int(kwargs['hgt'][:3])
+        if hgt < 150 or hgt > 193:
+            return False
+    else:
+        hgt = int(kwargs['hgt'][:2])
+        if hgt < 59 or hgt > 76:
+            return False
     return True
 
 def count_valid_passports(filename):
@@ -48,7 +91,8 @@ def count_valid_passports(filename):
         full_input = f.read()
         passport_inputs = full_input.split('\n\n')
 
-        valid_count = 0
+        part_1_count = 0
+        part_2_count = 0
         for passport_input in passport_inputs:
             kwarg_dict = {}
             input_pairs = (passport_input.replace('\n', ' ')).split()
@@ -57,12 +101,14 @@ def count_valid_passports(filename):
                 kwarg_dict[m.group('name')] = m.group('value')
             try:
                 Passport(**kwarg_dict)
-                valid_count += 1
+                part_1_count += 1
+                if passport_args_valid(kwarg_dict):
+                    part_2_count += 1
             except TypeError as e:
                 pass
-        return valid_count
+        print(part_1_count)
+        print(part_2_count)
 
 if __name__ == "__main__":
 
-    # Part 1:
-    print(count_valid_passports('./4/input.txt'))
+    count_valid_passports('./4/input.txt')
